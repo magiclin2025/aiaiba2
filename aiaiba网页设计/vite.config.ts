56bdde0411cbcd896280abb05e7063c51c1adffb
magiclin2025 <magiclin2025@outlook.com>
2026-07-31 00:45:37 +0800

    Delete aiaiba网页设计 directory

--- aiaiba网页设计/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  // 使用自定义域名 aiaiba.org，因此资源从域名根目录加载。
-  base: "/",
-});
